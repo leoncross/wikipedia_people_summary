d41193e24105cbf3e0be21bb96274254215745da
leoncross <leon@lscross.co.uk>
2019-03-05 00:26:13 +0000

    setup_variables function for interface

--- lib/app_interface.rb
+++ lib/app_interface.rb
@@ -3,15 +3,29 @@ require 'colorize'
 
 # handles the interface requirements
 class AppInterface
+  attr_reader :celebrity, :birthdate, :deathdate, :spouse_list, :article_summary
 
   def introduce
-    puts "Welcome, please enter the name of the celebrity you would like to search!".red
-    puts "At any time, type 'exit' to exit out of the app".red
-    puts "Accepted search format: 'Firstname Lastname' (e.g., John Lennon)".red
-    return gets.chomp
+    puts "Welcome, please enter the name of the celebrity you would like to search!",
+          "At any time, type 'exit' to exit out of the app",
+          "Accepted search format: 'Firstname Lastname' (e.g., John Lennon)"
+    gets.chomp
+  end
+
+  def setup_variables(celebrity, birthdate, deathdate, spouse_list, article_summary)
+    @celebrity = celebrity
+    @birthdate = birthdate
+    @deathdate = deathdate
+    @spouse_list = spouse_list
+    @article_summary = article_summary
+  end
+
+  def print_data
+    puts "Your searched celebrity details are as follows:",
+     "Birth date: #{@birthdate}",
+     "Death date: #{@deathdate}",
+     "Spouse: #{@spouse_list}",
+     "Article Summary #{@deathdate}"
   end
 
 end
-#
-# appin = AppInterface.new
-# appin.introduce
--- spec/app_interface_spec.rb
+++ spec/app_interface_spec.rb
@@ -3,11 +3,34 @@ require 'app_interface'
 describe AppInterface do
   let(:interface) { described_class.new }
 
-  context 'clean_search_result' do
-    it 'introduces user' do
+  context 'introduce' do
+    it 'introduces user and gives instructions' do
       allow(interface).to receive(:gets).and_return("celebrity")
-      allow(interface).to receive(:puts)
+      expect(interface).to receive(:puts).with("Welcome, please enter the name of the celebrity you would like to search!", "At any time, type 'exit' to exit out of the app", "Accepted search format: 'Firstname Lastname' (e.g., John Lennon)")
       expect(interface.introduce).to eq "celebrity"
+    end
+  end
+  context 'setup_variables' do
+    it 'assigns variable names' do
+      interface.setup_variables('celebrity','birthdate', 'deathdate', 'spouse_list', 'article_summary')
+      expect(interface.celebrity).to eq "celebrity"
+      expect(interface.birthdate).to eq "birthdate"
+      expect(interface.deathdate).to eq "deathdate"
+      expect(interface.spouse_list).to eq "spouse_list"
+      expect(interface.article_summary).to eq "article_summary"
+    end
+  end
+  context 'print_data' do
+    it 'prints the variables' do
+      interface.setup_variables('celebrity','birthdate', 'deathdate', 'spouse_list', 'article_summary')
+      expect(interface).to receive(:puts).with(
+          "Your searched celebrity details are as follows:",
+          "Birth date: birthdate",
+          "Death date: deathdate",
+          "Spouse: spouse_list",
+          "Article Summary deathdate"
+        )
+      interface.print_data
     end
   end
 end
